68e3fb6d0c1ec0a6893a799f0f3e708d473cf739
Yamnel <yamnel@users.noreply.github.com>
2017-04-01 21:44:32 -0400

    added IBM Connection

--- HertzChauffeur/HertzChauffeur/ViewController.swift
+++ HertzChauffeur/HertzChauffeur/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 import ConversationV1
 
-
+let username = "4fb1fe78-67be-4d52-9261-f44222e8a9b1"
+let password = "JWrL85EPu2Fh"
+let version = "2017-04-01" // use today's date for the most recent version
+let conversation = Conversation(username: username, password: password, version: version)
+let workspaceID = "0d9d120b-0dc3-4f04-8ff0-2b10320a5269"
+let failure = { (error: Error) in print(error) }
 
 class ViewController: UIViewController {
 
@@ -18,6 +23,24 @@ class ViewController: UIViewController {
         
         
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        var context: Context? // save context to continue conversation
+        conversation.message(withWorkspace: workspaceID, failure: failure) { response in
+            print(response.output.text)
+            context = response.context
+        }
+        
+        let text = "Turn the car on for me"
+        let request = MessageRequest(text: text, context: context)
+        conversation.message(withWorkspace: workspaceID, request: request, failure: failure) {
+            response in
+            print(response.output.text)
+            context = response.context
+        }
+        
+        print(context?.toJSON() ?? "Error")
+        
     }
 
     override func didReceiveMemoryWarning() {
