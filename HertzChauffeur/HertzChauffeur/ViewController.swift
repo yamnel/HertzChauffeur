fdecb9857681d13fecd4586b1e363f0bc419f876
Yamnel <yamnel@users.noreply.github.com>
2017-04-02 00:40:18 -0400

    integrated stephen's code

--- HertzChauffeur/HertzChauffeur/ViewController.swift
+++ HertzChauffeur/HertzChauffeur/ViewController.swift
@@ -9,36 +9,50 @@
 import UIKit
 import ConversationV1
 import Speech
+import RestKit
 
 
-//
+// IMB's Watson Credentials
 let username = "4fb1fe78-67be-4d52-9261-f44222e8a9b1"
 let password = "JWrL85EPu2Fh"
 let version = "2017-04-01" // use today's date for the most recent version
+// Start Conversation App
 let conversation = Conversation(username: username, password: password, version: version)
+
+// App ID
 let workspaceID = "0d9d120b-0dc3-4f04-8ff0-2b10320a5269"
-let failure = { (error: Error) in print(error) }
+let failure = { (error: Error) in print(error) } // for Fail message and logs
+
+
 
 class ViewController: UIViewController {
-    @IBOutlet weak var transcribeButton: UIButton!
+    
+    @IBOutlet weak var startBtn: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var myTextView: UITextView!
     
     private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
     
-    private var speechRecognitionRequest:
-    SFSpeechAudioBufferRecognitionRequest?
+    private var speechRecognitionRequest: SFSpeechAudioBufferRecognitionRequest?
     private var speechRecognitionTask: SFSpeechRecognitionTask?
     private let audioEngine = AVAudioEngine()
     private var textToSendToWatson: String = ""
-    private let commands: [String] = ["unlock my car"];
+    private var rawReplyFromWarson: Context?
+    
+    var systemToChange: String? // What we car system we want to change
+    var systemValue: String?  // What we are changing it to
+    
+    
+//    private let commands: [String] = ["unlock my car"];
     
     @IBAction func startTranscribing(_sender: AnyObject) {
-        transcribeButton.isEnabled = false;
+        startBtn.isEnabled = false;
         stopButton.isEnabled = true;
         try! startSession()
     }
     
+    
+    // Starts listening
     func startSession() throws {
         if let recognitionTask = speechRecognitionTask {
             recognitionTask.cancel()
@@ -65,7 +79,7 @@ class ViewController: UIViewController {
                 inputNode.removeTap(onBus: 0)
                 self.speechRecognitionRequest = nil
                 self.speechRecognitionTask = nil
-                self.transcribeButton.isEnabled = true
+                self.startBtn.isEnabled = true
             }
         }
         let recordingFormat = inputNode.outputFormat(forBus: 0)
@@ -76,56 +90,92 @@ class ViewController: UIViewController {
         
         audioEngine.prepare()
         try audioEngine.start()
-        
-        //get the audio engine to stop running
-        
     }
     
     @IBAction func stopTranscribing(_ sender: AnyObject) {
         if audioEngine.isRunning {
             audioEngine.stop()
             speechRecognitionRequest?.endAudio()
-            transcribeButton.isEnabled = true
+            startBtn.isEnabled = true
             stopButton.isEnabled = false
             
+            // Send to Watson.
             textToSendToWatson = myTextView.text.lowercased();
-            print(textToSendToWatson.lowercased());
-            print(validateCommand(textToValidate: textToSendToWatson.lowercased()))
+            
+            
+            
+            print(textToSendToWatson.lowercased()); // Testing
+//            print(validateCommand(textToValidate: textToSendToWatson.lowercased()))
+            
+            rawReplyFromWarson =  sendToWatson(voiceCommand: textToSendToWatson)
+            
         }
     }
     
-    func validateCommand(textToValidate: String) -> BooleanLiteralType {
-        for string in commands {
-            if textToValidate == string {
-                return true;
-            }
-        }
-        return false;
-    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        authorizeSpeechRecognition()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    func sendToWatson(voiceCommand: String) -> Context? {
+        var rawAnswer: Context?
         
-        
-        var context: Context? // save context to continue conversation
         conversation.message(withWorkspace: workspaceID, failure: failure) { response in
             print(response.output.text)
-            context = response.context
+            rawAnswer = response.context
         }
         
-        let text = "Turn the car on for me"
-        let request = MessageRequest(text: text, context: context)
+        let request = MessageRequest(text: voiceCommand, context: rawAnswer)
+        
         conversation.message(withWorkspace: workspaceID, request: request, failure: failure) {
             response in
             print(response.output.text)
-            context = response.context
+            rawAnswer = response.context
         }
         
-        print(context?.toJSON() ?? "Error")
+        return rawAnswer
+
+    }
+    
+    
+    
+//    func validateCommand(textToValidate: String) -> BooleanLiteralType {
+//        for string in commands {
+//            if textToValidate == string {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        authorizeSpeechRecognition()
+
+        
+//        var context: Context? // save context to continue conversation
+//        conversation.message(withWorkspace: workspaceID, failure: failure) { response in
+//            print(response.output.text)
+//            context = response.context
+//        }
+        
+//        let text = "Turn the car on for me"
+        
+//        let request = MessageRequest(text: text, context: context)
+//        conversation.message(withWorkspace: workspaceID, request: request, failure: failure) {
+//            response in
+//            print(response.output.text)
+//            context = response.context
+//        }
+//        
+//        print(context?.toJSON() ?? "")
         
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -138,16 +188,16 @@ class ViewController: UIViewController {
             OperationQueue.main.addOperation {
                 switch authStatus {
                 case .authorized:
-                    self.transcribeButton.isEnabled = true
+                    self.startBtn.isEnabled = true
                 case .denied:
-                    self.transcribeButton.isEnabled = false
-                    self.transcribeButton.setTitle("Speech recognition access denied by user", for: .disabled)
+                    self.startBtn.isEnabled = false
+                    self.startBtn.setTitle("Speech recognition access denied by user", for: .disabled)
                 case .restricted:
-                    self.transcribeButton.isEnabled = false
-                    self.transcribeButton.setTitle("Speech recognition restricted on, device", for: .disabled)
+                    self.startBtn.isEnabled = false
+                    self.startBtn.setTitle("Speech recognition restricted on, device", for: .disabled)
                 case .notDetermined:
-                    self.transcribeButton.isEnabled = false
-                    self.transcribeButton.setTitle("Speech recognition not authorized", for: .disabled)
+                    self.startBtn.isEnabled = false
+                    self.startBtn.setTitle("Speech recognition not authorized", for: .disabled)
                 }
             }
         }
